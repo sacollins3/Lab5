ad2f0ac8162abc6a305121c055e0a08989aa568c
sacollins3 <sacollins3@wisc.edu>
2021-10-21 19:35:25 -0500

    on "building third screen"

--- app/src/main/java/com/example/lab5/MainActivity2.java
+++ app/src/main/java/com/example/lab5/MainActivity2.java
@@ -5,14 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    public static ArrayList<Note> notes = new ArrayList<>();
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,13 +53,39 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        //display welcome message
         TextView textView = (TextView) findViewById(R.id.textView);
         Intent intent = getIntent();
-        //String str = intent.getStringExtra("username");
-        //welcomeText.setText("Welcome " + str); causing huge issues, null pointer exception?
         SharedPreferences sharedPreferences = getSharedPreferences("com.example.lab5", Context.MODE_PRIVATE);
         textView.setText("Welcome " + sharedPreferences.getString(MainActivity.usernameKey, ""));
 
+        //get SQLiteDatabase instance
+        Context context = getApplicationContext();
+        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("notes", Context.MODE_PRIVATE, null);
+        //initiate "notes" class var using readNotes method in DBHelper, use SharedPref username
+        DBHelper dbHelper = new DBHelper(sqLiteDatabase);
+        notes = dbHelper.readNotes(sharedPreferences.getString(MainActivity.usernameKey, ""));
+        //create arraylist<string> object by iterating over notes object
+        ArrayList<String> displayNotes = new ArrayList<>();
+        for (Note note : notes){
+            displayNotes.add(String.format("Title:%s\nDate:%s", note.getTitle(), note.getDate()));
+        }
+
+        //use ListView view to display notes on screen
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, displayNotes);
+        ListView listView = (ListView) findViewById(R.id.notesListView);
+        listView.setAdapter(adapter);
+
+        //add onItemClickListener for ListView item, a note in our case
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+                intent.putExtra("noteid", position);
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
